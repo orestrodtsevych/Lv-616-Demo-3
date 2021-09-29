baaf8d5904751798a018e5ba211b74ab437f92a6
Morten <zemik2004@gmail.com>
2021-09-29 13:42:37 +0300

    downloaded selenium packges

--- OpenCartSelenium/OpenCartSelenium/UnitTest1.cs
+++ OpenCartSelenium/OpenCartSelenium/UnitTest1.cs
@@ -1,18 +1,21 @@
 using NUnit.Framework;
-
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
 namespace OpenCartSelenium
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
+        public static ChromeOptions options = new ChromeOptions();
+        public IWebDriver driver = new ChromeDriver();
+        
         [Test]
         public void Test1()
-        {
-            Assert.Pass();
+        {       
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Navigate().GoToUrl("https://www.google.com.ua/");
         }
         public void Test2()
         {
