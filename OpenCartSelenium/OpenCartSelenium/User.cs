3db4528a1ef9472cf66c9d67fe3e95228c35deaa
Ігор Медвідь <Ihor.Medvid@lnu.edu.ua>
2021-10-03 12:41:43 +0300

    modified classes User and UserBuilder

--- OpenCartSelenium/OpenCartSelenium/User.cs
+++ OpenCartSelenium/OpenCartSelenium/User.cs
@@ -6,5 +6,26 @@ namespace OpenCartSelenium
 {
     class User
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string eMail { get; set; }
+        public string Telephone { get; set; }
+        public string MainAdress { get; set; }
+        public string City { get; set; }
+        public string PostCode { get; set; }
+        public string Country { get; set; }
+        public string RegionState { get; set; }
+        public string Password { get; set; }
+
+        public string Fax { get; set; }
+        public string Company { get; set; }
+        public string Address { get; set; }
+        public bool Subscribe { get; set; }
+
+        public User() { }
+        public static UserBuilder CreateBuilder()
+        {
+            return new UserBuilder();
+        }
     }
 }
--- OpenCartSelenium/OpenCartSelenium/UserBuilder.cs
+++ OpenCartSelenium/OpenCartSelenium/UserBuilder.cs
@@ -6,5 +6,84 @@ namespace OpenCartSelenium
 {
     class UserBuilder
     {
+        private User _user;
+        public UserBuilder()
+        {
+            _user = new User();
+        }
+        public UserBuilder SetFirstName(string firstName)
+        {
+            _user.FirstName = firstName;
+            return this;
+        }
+        public UserBuilder SetLastName(string lastName)
+        {
+            _user.LastName = lastName;
+            return this;
+        }
+        public UserBuilder SetEMail(string email)
+        {
+            _user.eMail = email;
+            return this;
+        }
+        public UserBuilder SetTelephone(string telephone)
+        {
+            _user.Telephone = telephone;
+            return this;
+        }
+        public UserBuilder SetMainAdress(string adress)
+        {
+            _user.MainAdress = adress;
+            return this;
+        }
+        public UserBuilder SetCity(string city)
+        {
+            _user.City = city;
+            return this;
+        }
+        public UserBuilder SetPostCode(string code)
+        {
+            _user.PostCode = code;
+            return this;
+        }
+        public UserBuilder SetCountry(string country)
+        {
+            _user.Country = country;
+            return this;
+        }
+        public UserBuilder SetRegionState(string regionState)
+        {
+            _user.RegionState = regionState;
+            return this;
+        }
+        public UserBuilder SetPassword(string password)
+        {
+            _user.Password = password;
+            return this;
+        }
+        public UserBuilder SetFax(string fax)
+        {
+            _user.Fax = fax;
+            return this;
+        }
+        public UserBuilder SetCompany(string company)
+        {
+            _user.Company = company;
+            return this;
+        }
+        public UserBuilder SetAdress(string adress)
+        {
+            _user.Address = adress;
+            return this;
+        }
+        public UserBuilder SetSubscribe(bool subscribe)
+        {
+            _user.Subscribe = subscribe;
+            return this;
+        }
+        public User Build()
+        {
+            return _user;
+        }
     }
 }
