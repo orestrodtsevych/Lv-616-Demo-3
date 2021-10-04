ff8d895a73b79bb46a972005957fe4a31e8d521c
Vitalii Baryliak <vitalik27578@gmail.com>
2021-10-04 23:59:11 +0300

    Modified class MyaccountPage

--- OpenCartSelenium/OpenCartSelenium/MyAccountPage.cs
+++ OpenCartSelenium/OpenCartSelenium/MyAccountPage.cs
@@ -7,11 +7,14 @@ namespace OpenCartSelenium
 {
     public class MyAccountPage : ARightMenuComponent
     {
+        public IWebElement MyAccountLabel { get; private set; }
         public IWebElement EditAccountInformation { get; private set; }
         public IWebElement ChangeYourPassword { get; private set; }
         public MyAccountPage(IWebDriver driver) : base(driver)
         {
+            MyAccountLabel = driver.FindElement(By.XPath("//*[@id='content']/h2[1]"));
         }
+        public string GetMyAccountLabelText() => MyAccountLabel.Text;
         public string GetEditAccountInformationText() => EditAccountInformation.Text;
         public void ClickEditAccountInformation() => EditAccountInformation.Click();
         public string GetChangeYourPasswordText() => ChangeYourPassword.Text;
