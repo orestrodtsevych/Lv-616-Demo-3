c7bb28ef7ec7505a65120de526f1a3f60a173045
Vitalii Baryliak <vitalik27578@gmail.com>
2021-10-04 15:11:38 +0300

    New Classes has been added

---
+++ OpenCartSelenium/OpenCartSelenium/AUnloggedRightMenuComponent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using OpenQA.Selenium;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OpenCartSelenium
+{
+    class AUnloggedRightMenuComponent : AStatusBarComponent
+    {
+        public AUnloggedRightMenuComponent(IWebDriver driver) : base(driver)
+        { }
+    }
+}
---
+++ OpenCartSelenium/OpenCartSelenium/LoginPage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using OpenQA.Selenium;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OpenCartSelenium
+{
+    class LoginPage
+    {
+        public IWebElement EmailField { get; private set; }
+        public IWebElement PasswordField { get; private set; }
+        public LoginPage(IWebDriver driver) : base(driver)
+        {
+            InitLoginComponent();
+        }
+        private void InitLoginComponent()
+        {
+            EmailField = driver.FindElement(By.Id("input-email"));
+            PasswordField = driver.FindElement(By.Id("input-password"));
+        }
+        public void ClickEmailField() => EmailField.Click();
+        public void ClickPasswordField() => PasswordField.Click();
+
+    }
+}
