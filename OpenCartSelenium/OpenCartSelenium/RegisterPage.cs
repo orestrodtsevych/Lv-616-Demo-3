b115cc9d8c5697c3f2588abccf627554a5cfd4f2
Ігор Медвідь <Ihor.Medvid@lnu.edu.ua>
2021-10-05 11:38:07 +0300

    added class RegisterPage

---
+++ OpenCartSelenium/OpenCartSelenium/RegisterPage.cs
@@ -0,0 +1,46 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenCartSelenium
+{
+    class RegisterPage: AUnloggedRightMenuComponent
+    {
+        public IWebElement FirstNameField;
+        public IWebElement LastNameField;
+        public IWebElement EMailField;
+        public IWebElement TelephoneField;
+        public IWebElement PasswordField;
+        public IWebElement ConfirmPasswordField;
+        public IWebElement SubscribeRadio;
+
+
+        public RegisterPage(IWebDriver driver): base(driver)
+        {
+            InitialRegisterPage();
+        }
+
+        public void InitialRegisterPage()
+        {
+            FirstNameField = driver.FindElement(By.Id("input-firstname"));
+            LastNameField = driver.FindElement(By.Id("input-lastname"));
+            EMailField = driver.FindElement(By.Id("input-email"));
+            TelephoneField = driver.FindElement(By.Id("input-telephone"));
+            PasswordField = driver.FindElement(By.Id("input-password"));
+            ConfirmPasswordField = driver.FindElement(By.Id("input-confirm"));
+        }
+
+        public void ClickFirstNameField() => FirstNameField.Click();
+        public void ClickLastNameField() => LastNameField.Click();
+        public void ClickEMailField() => EMailField.Click();
+        public void ClickTelephoneField() => TelephoneField.Click();
+        public void ClickPasswordField() => PasswordField.Click();
+        public void ClickConfirmPasswordField() => ConfirmPasswordField.Click();
+
+
+
+
+
+    }
+}
