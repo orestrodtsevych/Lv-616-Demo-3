e5c674a35673300e4ffcab6e5559676933ea7698
Yevheniy <peredaryuk.yevheniy@gmail.com>
2021-10-07 15:45:27 +0300

    Modified class ProductsListComponent

--- OpenCartSelenium/OpenCartSelenium/ProductsListComponent.cs
+++ OpenCartSelenium/OpenCartSelenium/ProductsListComponent.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using OpenQA.Selenium;
 using System.Collections.Generic;
 using System.Text;
@@ -9,5 +10,22 @@ namespace OpenCartSelenium
 {
     class ProductsListComponent
     {
+        private IWebDriver _driver;
+        private List<ProductComponent> _productComponents;
+
+        public ProductsListComponent(IWebDriver driver)
+        {
+            _driver = driver;
+            initProductsListComponents();
+        }
+
+        public List<ProductComponent> ProductComponents => _productComponents;
+
+        private void initProductsListComponents()
+        {
+            _productComponents = new List<ProductComponent>();
+            foreach (var item in _driver.FindElements(By.CssSelector(".product-layout")))
+                _productComponents.Add(new ProductComponent(item));
+        }
     }
 }
