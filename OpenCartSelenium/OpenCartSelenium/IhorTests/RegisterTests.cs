41c8d96f885e1c32158e58383f9508b2b3cc801b
Ігор Медвідь <Ihor.Medvid@lnu.edu.ua>
2021-10-07 19:50:15 +0300

    added Register page tests

--- OpenCartSelenium/OpenCartSelenium/IhorTests/RegisterTests.cs
+++ OpenCartSelenium/OpenCartSelenium/IhorTests/RegisterTests.cs
@@ -16,19 +16,23 @@ namespace OpenCartSelenium.IhorTests
         IWebDriver driver;
         private readonly string OpenCartURL = "http://192.168.1.103/opencart/upload/index.php?route=account/register";
         [OneTimeSetUp]
-        public void SetUp()
+        public void BeforeAllMethods()
         {
             driver = new ChromeDriver();
-
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1);
         }
-        [Test]
-        public void RegisterNewUser()
+        [SetUp]
+        public void SetUp()
         {
             driver.Manage().Window.Maximize();
             driver.Navigate().GoToUrl(OpenCartURL);
-            
+        }
+
+        [Test]
+        public void RegisterNewUser()
+        {
             RegisterPage registerPage = new RegisterPage(driver);
-            User newUser = User.CreateBuilder().SetFirstName("roman").SetLastName("romaniv").SetEMail("rom12333an@gmal.com")
+            User newUser = User.CreateBuilder().SetFirstName("roman").SetLastName("romaniv").SetEMail("rom123334an@gmal.com")
                 .SetTelephone("380676767235").SetPassword("passwrd").Build();
             
             registerPage.FillRegisterForm(newUser);
@@ -44,8 +48,6 @@ namespace OpenCartSelenium.IhorTests
         [Test]
         public void RegisterShowMessageAboutEMail()
         {
-            driver.Manage().Window.Maximize();
-            driver.Navigate().GoToUrl(OpenCartURL);
             RegisterPage registerPage = new RegisterPage(driver);
             User newUser = User.CreateBuilder().SetFirstName("roman").SetLastName("romaniv").SetEMail("roman@gmal.com")
                 .SetTelephone("380676767235").SetPassword("passwrd").Build();
@@ -62,20 +64,68 @@ namespace OpenCartSelenium.IhorTests
         [Test]
         public void RegisterShowMessageAboutAgreePrivacyPolicy()
         {
-            driver.Manage().Window.Maximize();
-            driver.Navigate().GoToUrl(OpenCartURL);
             RegisterPage registerPage = new RegisterPage(driver);
             User newUser = User.CreateBuilder().SetFirstName("roman").SetLastName("romaniv").SetEMail("roman@gmal.com")
                 .SetTelephone("380676767235").SetPassword("passwrd").Build();
 
             registerPage.FillRegisterForm(newUser);
-            registerPage.ClickAgreeCheckBox();
             registerPage.ClickContinueButton();
 
             RegisterMessagePage messagePage = new RegisterMessagePage(driver);
             string expected = messagePage.EXPECTED_WARNING_AGREE;
             string actual = messagePage.GetAlertMessageText();
             Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void RegisterValidMessage()
+        {
+            RegisterPage registerPage = new RegisterPage(driver);
+            User newUser = new UserBuilder().SetFirstName(string.Empty).SetLastName(string.Empty).SetEMail(string.Empty)
+                .SetTelephone(string.Empty).SetPassword(string.Empty).Build();
+
+            registerPage.FillRegisterForm(newUser);
+            registerPage.ClickAgreeCheckBox();
+            registerPage.ClickContinueButton();
+
+            RegisterTextDanger registerTextDanger = new RegisterTextDanger(driver);
+            string expectedFirstNameTextDanger = registerTextDanger.EXPECTED_VALID_FIRSTNAME_MESSAGE;
+            string actualFirstNameTextDanger = registerTextDanger.GetFirstNameTextDanger();
+
+            string expectedLastNameTextDanger = registerTextDanger.EXPECTED_VALID_LASTNAME_MESSAGE;
+            string actualLastNameTextDanger = registerTextDanger.GetLastNameTextDanger();
+
+            string expectedEMailTextDanger = registerTextDanger.EXPECTED_VALID_EMAIL_MESSAGE;
+            string actualEMailTextDanger = registerTextDanger.GetEMailTextDanger();
+
+            string expectedTelephoneTextDanger = registerTextDanger.EXPECTED_VALID_TELEPHONE_MESSAGE;
+            string actualTelephoneTextDanger = registerTextDanger.GetTelephoneTextDanger();
+
+            string expectedPasswordTextDanger = registerTextDanger.EXPECTED_VALID_PASSWORD_MESSAGE;
+            string actualPasswordTextDanger = registerTextDanger.GetPasswordTextDanger();
+
+            Assert.AreEqual(expectedFirstNameTextDanger, actualFirstNameTextDanger);
+            Assert.AreEqual(expectedLastNameTextDanger, actualLastNameTextDanger);
+            Assert.AreEqual(expectedEMailTextDanger, actualEMailTextDanger);
+            Assert.AreEqual(expectedTelephoneTextDanger, actualTelephoneTextDanger);
+            Assert.AreEqual(expectedPasswordTextDanger, actualPasswordTextDanger);
+        }
+        [Test]
+        public void ConfirmPasswordTest()
+        {
+            RegisterPage registerPage = new RegisterPage(driver);
+            User newUser = User.CreateBuilder().SetFirstName("roman").SetLastName("romaniv").SetEMail("roman34@gmal.com")
+                .SetTelephone("380676767235").SetPassword("passwrd").Build();
+
+            registerPage.FillRegisterForm(newUser);
+            registerPage.SetConfirmPasswordField("notconfirm");
+            registerPage.ClickAgreeCheckBox();
+            registerPage.ClickContinueButton();
+
+            RegisterTextDanger registerTextDanger = new RegisterTextDanger(driver);
+            string expectedConfirmTextDanger = registerTextDanger.EXPECTED_VALID_CONFIRMPASSWORD_MESSAGE;
+            string actualConfirmTextDanger = registerTextDanger.GetConfirmPasswordTextDanger();
+            Assert.AreEqual(expectedConfirmTextDanger, actualConfirmTextDanger);
+
         }
     }
 }
