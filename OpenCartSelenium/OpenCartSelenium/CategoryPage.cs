dc0ba88d537a8c7882405fb3c21226de3269f205
Yevheniy <peredaryuk.yevheniy@gmail.com>
2021-10-07 19:41:22 +0300

    Added new page class for Caregories

---
+++ OpenCartSelenium/OpenCartSelenium/CategoryPage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using OpenQA.Selenium;
+
+namespace OpenCartSelenium
+{
+    class CategoryPage : AStatusBarComponent
+    {
+        private IWebElement _productCompare;
+        public CategoryPage(IWebDriver driver) : base(driver) 
+        {
+            _productCompare = driver.FindElement(By.Id("compare-total"));
+        }
+
+        public string ProductCompareText => _productCompare.Text;
+
+        public void ClickOnProductCompare()
+            => _productCompare.Click();
+    }
+}
