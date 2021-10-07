7e2639a458120f12c87c772c01e84dd3ffa20302
Yevheniy <peredaryuk.yevheniy@gmail.com>
2021-10-07 14:51:05 +0300

    Added new class for list of products

---
+++ OpenCartSelenium/OpenCartSelenium/ProductsListComponent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using OpenQA.Selenium;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+
+namespace OpenCartSelenium
+{
+    class ProductsListComponent
+    {
+    }
+}
