1b4eb6de86b727f260c8c831389755e1bb3d76db
Ігор Медвідь <Ihor.Medvid@lnu.edu.ua>
2021-10-10 13:49:50 +0300

    added class MyAccountMessagePage

---
+++ OpenCartSelenium/OpenCartSelenium/MyAccountMessagePage.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenCartSelenium
+{
+    class MyAccountMessagePage: MyAccountPage
+    {
+        public readonly string EXPECTED_SUCCESS_MESSAGE = "Success: Your account has been successfully updated.";
+
+        public IWebElement AlertMessage { get; private set; }
+
+        public MyAccountMessagePage(IWebDriver driver): base(driver)
+        {
+            InitAlertMessage();
+        }
+
+        public void InitAlertMessage()
+        {
+            AlertMessage = driver.FindElement(By.CssSelector(".alert.alert-success"));
+        }
+
+        public string GetAlertMessageText() => AlertMessage.Text;
+    }
+}
