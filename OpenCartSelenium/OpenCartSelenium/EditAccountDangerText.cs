2e351ed90c8c14a2c7c2d0a2a76a9c54835d29d5
Ігор Медвідь <Ihor.Medvid@lnu.edu.ua>
2021-10-10 13:48:47 +0300

    added class EditAccountDangerText

---
+++ OpenCartSelenium/OpenCartSelenium/EditAccountDangerText.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenCartSelenium
+{
+    class EditAccountDangerText: EditAccountPage
+    {
+        public readonly string EXPECTED_VALID_FIRSTNAME_MESSAGE = "First Name must be between 1 and 32 characters!";
+        public readonly string EXPECTED_VALID_LASTNAME_MESSAGE = "Last Name must be between 1 and 32 characters!";
+        public readonly string EXPECTED_VALID_EMAIL_MESSAGE = "E-Mail Address does not appear to be valid!";
+        public readonly string EXPECTED_VALID_TELEPHONE_MESSAGE = "Telephone must be between 3 and 32 characters!";
+        public IList<IWebElement> TextDangerMessages { get; private set; }
+
+        public EditAccountDangerText(IWebDriver driver):base(driver)
+        {
+            TextDangerMessages = driver.FindElements(By.CssSelector(".text-danger"));
+        }
+        public string GetFirstNameTextDanger() => TextDangerMessages.Select(x => x.FindElement(By.XPath("//*[@id='input-firstname']/following-sibling::div"))).FirstOrDefault().Text;
+        public string GetLastNameTextDanger() => TextDangerMessages.Select(x => x.FindElement(By.XPath("//*[@id='input-lastname']/following-sibling::div"))).FirstOrDefault().Text;
+        public string GetEMailTextDanger() => TextDangerMessages.Select(x => x.FindElement(By.XPath("//*[@id='input-email']/following-sibling::div"))).FirstOrDefault().Text;
+        public string GetTelephoneTextDanger() => TextDangerMessages.Select(x => x.FindElement(By.XPath("//*[@id='input-telephone']/following-sibling::div"))).FirstOrDefault().Text;
+    }
+}
